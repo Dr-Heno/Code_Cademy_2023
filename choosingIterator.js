811b665e90a39c44b537c2b322ba8290ea0cf804
Rick Heno <henophotography@gmail.com>
2023-02-08 16:08:37 -0600

    Added #

--- choosingIterator.js
+++ choosingIterator.js
@@ -1,6 +1,6 @@
 const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
 
-const nums = [1, 50, 75, 200, 350, 525, 1000];
+const nums = [1, 50, 75, 200, 350, 525, 1000, 2000];
 
 //  Choose a method that will return undefined
 cities.forEach(city => console.log('Have you visited ' + city + '?'));
